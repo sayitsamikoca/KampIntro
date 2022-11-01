7584e9ad2a089a5f87355d63ff0e9e3e8a838b71
Sayit Sami Koca <sayitsamikoca0@gmail.com>
2022-11-01 06:08:22 +0300

    Kamp 4. Gün Ders Sonu.

---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace GenericsIntro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> isimler = new MyList<string>();
+
+            isimler.Add("Sayit");
+            Console.WriteLine(isimler.Length);
+
+            isimler.Add("Lerdorf");
+            Console.WriteLine(isimler.Length);
+
+
+            foreach (var isim in isimler.Items)
+            {
+                Console.WriteLine(isim);
+            }
+        }
+    }
+}
---
+++ OOP1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace OOP1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.CategoryId = 2;  //CategoryId=2 Mobilya Kategorisidir.
+            product1.ProductName = "Masa";
+            product1.UnitPrice = 500;
+            product1.UnitsInStock = 3;
+
+            Product product2 = new Product() { Id = 2, CategoryId = 5, ProductName = "Kalem", UnitPrice = 35, UnitsInStock = 5 };
+
+            //case sensitive
+            //PascalCase   //camelCase
+            ProductManager productManager = new ProductManager(); //101
+            productManager.Add(product1);
+
+            Console.WriteLine(product1.ProductName); 
+
+
+
+            // int,double,bool... değer tip
+            // diziler,class,abstract class, interface ... referans tip
+            // ref-out
+        }
+    }
+}
